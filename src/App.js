801e9db3cbdfbf16f92c231ae22628d8980467fd
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-07-20 15:57:58 +0530

    update UI based on the token

--- src/App.js
+++ src/App.js
@@ -15,12 +15,15 @@ import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import { action as logoutAction } from "./pages/Logout";
+import { tokenLoader } from "./util/auth";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
